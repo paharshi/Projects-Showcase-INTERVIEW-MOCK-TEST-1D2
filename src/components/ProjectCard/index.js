e5eeced6d12df74f1f289a40b4c39ad88098ed49
paharshi <paharshi.madasu@gmail.com>
2023-02-08 04:18:34 +0000

    solution

---
+++ src/components/ProjectCard/index.js
@@ -0,0 +1,15 @@
+import {ProjectCardListItem, ProjectCardImage, ProjectCardName} from './style'
+
+const ProjectCard = props => {
+  const {projectCardDetails} = props
+  const {name, imageUrl} = projectCardDetails
+
+  return (
+    <ProjectCardListItem>
+      <ProjectCardImage src={imageUrl} alt={name} />
+      <ProjectCardName>{name}</ProjectCardName>
+    </ProjectCardListItem>
+  )
+}
+
+export default ProjectCard
